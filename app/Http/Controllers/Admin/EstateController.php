9d0a195564e3f827d226e54a7a2edc4c066db822
fabriziom90 <fabriziomastrobattista@gmail.com>
2024-12-03 11:52:35 +0100

    [UPD] Created models, migrations, seeders, resource controllers and models requests

---
+++ app/Http/Controllers/Admin/EstateController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Estate;
+use App\Http\Requests\StoreEstateRequest;
+use App\Http\Requests\UpdateEstateRequest;
+use App\Http\Controllers\Controller;
+
+class EstateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEstateRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Estate $estate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Estate $estate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEstateRequest $request, Estate $estate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Estate $estate)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Admin/GalleryImageController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\GalleryImage;
+use App\Http\Requests\StoreGalleryImageRequest;
+use App\Http\Requests\UpdateGalleryImageRequest;
+use App\Http\Controllers\Controller;
+
+class GalleryImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreGalleryImageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GalleryImage $galleryImage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(GalleryImage $galleryImage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateGalleryImageRequest $request, GalleryImage $galleryImage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(GalleryImage $galleryImage)
+    {
+        //
+    }
+}
