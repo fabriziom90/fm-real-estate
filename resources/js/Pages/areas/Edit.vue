82e100303e7ddd9c373459241b66ab708192a8cd
fabriziom90 <fabriziomastrobattista@gmail.com>
2024-12-16 13:10:43 +0100

    [UPD] Minor fixes for table

---
+++ resources/js/Pages/areas/Edit.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { ref } from "vue";
+
+import axios from "axios";
+
+import AuthenticatedLayout from "../../Layouts/AuthenticatedLayout.vue";
+import TextInput from "../../Components/TextInput.vue";
+import { useForm } from "@inertiajs/vue3";
+import { useToast } from "vue-toast-notification";
+
+defineProps({ errors: Object });
+
+let area = ref("");
+
+const $toast = useToast();
+
+const form = useForm({
+    area: "",
+});
+
+const store = () => {
+    form.post(route("admin.areas.store"), {
+        onSuccess: (resp) => {
+            $toast.success("Area creata", {
+                position: "top-right",
+                duration: 3000,
+            });
+        },
+        onError: (resp) => {
+            $toast.error(
+                "Inserimento non completato. Verifica i campi della form.",
+                {
+                    position: "top-right",
+                    duration: 3000,
+                }
+            );
+        },
+    });
+};
+</script>
+<template lang="">
+    <AuthenticatedLayout>
+        <div class="flex justify-between items-center">
+            <h2 class="text-3xl mb-10">Aggiungi Area</h2>
+            <a
+                :href="route('admin.areas.index')"
+                class="tool-button bg-main-blue border-main-blue text-white"
+            >
+                Visualizza aree
+            </a>
+        </div>
+        <div class="nuova-form">
+            <form @submit.prevent="submit">
+                <div class="grid grid-cols-3">
+                    <div>
+                        <label class="label-text">Nome area</label>
+                        <input
+                            type="text"
+                            placeholder="Nome area"
+                            class="input-sm rounded w-full border-gray-300 shadow-sm focus:border-main-blue focus:ring-main-blue"
+                            v-model="form.area"
+                        />
+                        <div v-if="form.errors.area" class="text-danger">
+                            {{ form.errors.area }}
+                        </div>
+                    </div>
+                </div>
+                <div class="grid grid-cols-10 mt-10">
+                    <button
+                        @click="store"
+                        :disabled="form.processing"
+                        class="tool-button bg-main-blue border-main-blue text-white m-0"
+                    >
+                        {{ form.processing ? "Salvataggio in corso" : "Salva" }}
+                    </button>
+                </div>
+            </form>
+        </div>
+    </AuthenticatedLayout>
+</template>
+<style lang=""></style>
--- resources/js/Pages/areas/Index.vue
+++ resources/js/Pages/areas/Index.vue
@@ -1,25 +1,22 @@
 <script setup>
 import { ref, onMounted, computed } from "vue";
 import AuthenticatedLayout from "../../Layouts/AuthenticatedLayout.vue";
-import Table from '../../Components/Table.vue';
+import Table from "../../Components/Table.vue";
 
 import { usePage } from "@inertiajs/vue3";
-import Pagination from '../../Components/Pagination.vue';
+import Pagination from "../../Components/Pagination.vue";
 
 defineProps({
     message: String,
 });
 
-
 let areas = ref([]);
-const columns = ['ID', 'Nome area', 'Tools'];
+const columns = ["ID", "Nome area", "Tools"];
 
 onMounted(() => {
     const { props } = usePage();
     areas.value = props.areas;
 });
-
-
 </script>
 
 <template lang="">
@@ -33,30 +30,8 @@ onMounted(() => {
                 Aggiungi area
             </a>
         </div>
-        
-        <Table :rows="areas" :columns="columns">
-            <td>
-                <a
-                    href="#"
-                    class="tool-button text-white bg-main-blue border-main-blue"
-                >
-                    <i class="fas fa-eye"></i>
-                </a>
-                <a
-                    href="#"
-                    class="tool-button text-white bg-warning border-warning"
-                >
-                    <i class="fas fa-edit"></i>
-                </a>
-                <a
-                    href="#"
-                    class="tool-button text-white bg-danger border-danger"
-                >
-                    <i class="fas fa-trash"></i>
-                </a>
-            </td>
-        </Table>
-        
+
+        <Table :rows="areas" :columns="columns" />
     </AuthenticatedLayout>
 </template>
 <style lang=""></style>
